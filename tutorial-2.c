81f3a175050c8caa19650871bda980cf9dd2a383
andrewLivancov <gilamrot@gmail.com>
2020-07-17 23:04:24 +0300

    using int and float cpecificators

---
+++ tutorial-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t;
+    float v;
+    t = 15;
+    t = (t - 2) * t + 5;
+    v = 2.54;
+    v = t;
+    printf("%g", v);
+    return 0;
+}
