e9cea9e1652b8115035aa866784de77e2b837478
andrewLivancov <gilamrot@gmail.com>
2020-07-18 13:47:55 +0300

    modified if else sample

---
+++ tutorial-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t;
+    t = 35;
+    if (t > 10)
+    {
+        printf("www\n");
+        if (t > 20)
+            printf("123\n");
+        if (t > 30)
+            printf("sss%d", t);
+                return 2;
+    }
+    else
+    {
+        printf("ggg");
+        return 7;
+    }
+}
