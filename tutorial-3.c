69017171e1902dcae4392243dfedec78b633b59f
andrewLivancov <gilamrot@gmail.com>
2020-07-18 13:01:39 +0300

    if else operators

---
+++ tutorial-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int t;
+    t = 25;
+    if (t > 10)
+    {
+      printf("www");
+      if (t > 20)
+             printf("123");
+    }
+    else printf("ggg");
+    return 2;
+}
